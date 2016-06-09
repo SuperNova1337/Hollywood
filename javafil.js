812698c852d1eeca1cb94188eeb56bd40f8e398d
SuperNova1337 <ljungberg.skol@gmail.com>
2016-06-09 22:25:04 +0200

    Har gjort ändringar för att mobilanpassa sidan

--- javafil.js
+++ javafil.js
@@ -1,4 +1,4 @@
-<script>
+
 		function detectmob() { 
 				 if( navigator.userAgent.match(/Android/i)
 				  || navigator.userAgent.match(/webOS/i)
@@ -11,38 +11,17 @@
 				 else {return false;}
 				}
 		
-		function checkPhone(){ //Anropa funktionen checkPhone() med <body onLoad="checkPhone()">
-
-			if(detectmob()){ 
-			  // Kod för att mobilanpassa sidan
-				
-				// Exempelvis:
-  				element=document.getElementById("test");
-  				deals = element.childNodes[7];
-  				diablo = element.childNodes[11];
-  				
-  				element.insertBefore(deals,diablo);
-			}
-		}
-		</script>
 
 
-		function detectmob() { 
-           if( navigator.userAgent.match(/Android/i)
-            || navigator.userAgent.match(/webOS/i)
-            || navigator.userAgent.match(/iPhone/i)
-            || navigator.userAgent.match(/iPad/i)
-            || navigator.userAgent.match(/iPod/i)
-            || navigator.userAgent.match(/BlackBerry/i)
-            || navigator.userAgent.match(/Windows Phone/i)
-           ){return true;}
-           else {return false;}
-       }
 
-
-function onSiteLoad(id){
+		function onSiteLoad(){
     if(detectmob()){
 
-       document.getElementById("right1").style.marginLeft = " 420px";
+       document.getElementById("wrapper1").style.marginLeft = " 420px";
     }
 }
+		
+
+
+
+
